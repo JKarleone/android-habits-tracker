37a1190652e26a9b0a0924d38c9b39b9b20831ff
Maxim Zadumin <recentfear@yandex.ru>
2021-05-12 17:46:03 +0500

    Added simple header with profile image to NavigationView

--- app/src/main/java/com/example/habitstracker/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/habitstracker/presentation/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.habitstracker.presentation.main
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -8,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.bumptech.glide.Glide
 import com.example.habitstracker.R
 import com.example.habitstracker.databinding.ActivityMainBinding
 
@@ -30,10 +32,29 @@ class MainActivity : AppCompatActivity() {
         )
         binding.navigationView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfig)
+
+        setupProfileImage()
+    }
+
+    fun setupProfileImage() {
+        val headerView = binding.navigationView.getHeaderView(0)
+        val profileImage = headerView.findViewById<ImageView>(R.id.profileImageView)
+
+        Glide
+            .with(this)
+            .load(PROFILE_IMAGE_URL)
+            .fitCenter()
+            .into(profileImage)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
+    }
+
+    companion object {
+
+        private const val PROFILE_IMAGE_URL = "https://yar-citi-dog.ru/wp-content/uploads/siba-inu-foto26.jpg"
+
     }
 
 }
